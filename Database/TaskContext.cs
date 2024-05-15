6331a0c44563c232018bce210c228756d6dec8d1
Patrick Patchelli <patrick.patchelli@hotmail.com>
2024-05-15 12:52:18 -0300

    create migration and identity

--- Database/TaskContext.cs
+++ Database/TaskContext.cs
@@ -1,31 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ToDo.API.Models;
 
 namespace ToDo.API.Database
 {
-    public class TaskContext : DbContext
+    public class TaskContext : IdentityDbContext<ApplicationUser>
     {
         public TaskContext(DbContextOptions<TaskContext> options) : base(options)
         {
         }
 
-        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<ToDo.API.Models.Task> Tasks { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-
-            modelBuilder.Entity<ApplicationUser>()
-                .HasMany(u => u.Tasks)
-                .WithOne(t => t.User)
-                .HasForeignKey(t => t.UserId);
-
-            modelBuilder.Entity<ToDo.API.Models.Task>()
-                .HasOne(t => t.User)
-                .WithMany(u => u.Tasks)
-                .HasForeignKey(t => t.UserId);
-        }
     }
 }
